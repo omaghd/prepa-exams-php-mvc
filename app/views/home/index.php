8f46927e1728999c9d990eea93d074c0bca17f95
Omar EL ATMANI <contact@omaghd.com>
2021-02-06 03:54:03 +0100

    Design home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,6 +1,19 @@
     <?php
     ?>
 
-    <div class="container page-wrap">
-        <i class="fas fa-home mr-2"></i>Welcome Home!
+    <div class="page-wrap d-flex flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <h1 class="display-4 mb-5">Happy Learning!</h1>
+
+                    <div class="lead">Choisissez un niveau!</div>
+                    <div class="btn-group" role="group">
+                        <a href="<?= URLROOT . '/grade/first' ?>" class="btn btn-info btn-lg text-uppercase">1ère année</a>
+                        <a href="<?= URLROOT . '/grade/second' ?>" class="btn btn-info btn-lg text-uppercase">2ème année</a>
+                        <a href="<?= URLROOT . '/grade/third' ?>" class="btn btn-info btn-lg text-uppercase">3ème année</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
