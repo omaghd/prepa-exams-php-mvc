054db7d3a862baafb5d73be1bfa41dff352842b8
Omar EL ATMANI <contact@omaghd.com>
2021-02-06 05:00:51 +0100

    Design a responsive mockup for "3ème Année" page

--- app/views/grades/third.php
+++ app/views/grades/third.php
@@ -1,1 +1,166 @@
-Third
+    <?php
+    ?>
+
+    <div class="d-flex flex-row align-items-center mt-3">
+        <div class="container">
+            <div class="d-md-flex">
+                <div class="list-group col-sm-12 col-md-5 col-lg-4 ml-2 ml-md-0">
+                    <h3>Matières</h3>
+                    <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex d-block justify-content-between align-items-center">
+                            <div>
+                                PHP avancé
+                            </div>
+                            <div>
+                                <div class="btn-group" role="group" aria-label="btns">
+                                    <button type="button" class="btn btn-light btn-sm">Controles</button>
+                                    <button type="button" class="btn btn-light btn-sm">Examens</button>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex d-block justify-content-between align-items-center">
+                            <div>
+                                Génie Logiciel
+                            </div>
+                            <div>
+                                <div class="btn-group" role="group" aria-label="btns">
+                                    <button type="button" class="btn btn-light btn-sm">Controles</button>
+                                    <button type="button" class="btn btn-light btn-sm">Examens</button>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex d-block justify-content-between align-items-center">
+                            <div>
+                                UML
+                            </div>
+                            <div>
+                                <div class="btn-group" role="group" aria-label="btns">
+                                    <button type="button" class="btn btn-light btn-sm">Controles</button>
+                                    <button type="button" class="btn btn-light btn-sm">Examens</button>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action active">
+                        <div class="d-flex d-block justify-content-between align-items-center">
+                            <div>
+                                Java
+                            </div>
+                            <div>
+                                <div class="btn-group" role="group" aria-label="btns">
+                                    <button type="button" class="btn btn-light btn-sm">Controles</button>
+                                    <button type="button" class="btn btn-light btn-sm">Examens</button>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex d-block justify-content-between align-items-center">
+                            <div>
+                                Meth. Design
+                            </div>
+                            <div>
+                                <div class="btn-group" role="group" aria-label="btns">
+                                    <button type="button" class="btn btn-light btn-sm">Controles</button>
+                                    <button type="button" class="btn btn-light btn-sm">Examens</button>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="list-group col-sm col-md col-lg ml-2 ml-md-0 mt-md-0 mt-2">
+                    <h3>Examens</h3>
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#twenty">2019-2020</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#nineteen">2018-2019</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#eighteen">2017-2018</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#seventeen">2016-2017</a>
+                        </li>
+                    </ul>
+                    <div id="myTabContent" class="tab-content">
+                        <div class="tab-pane fade" id="twenty">
+                            <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+                        </div>
+                        <div class="tab-pane fade active show" id="nineteen">
+                            <div class="accordion mt-3" id="parties">
+                                <div class="card">
+                                    <div class="card-header py-1" id="partie_1">
+                                        <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            <h5>
+                                                Partie 1:
+                                            </h5>
+                                        </button>
+                                    </div>
+
+                                    <div id="collapseOne" class="collapse show" aria-labelledby="partie_1" data-parent="#parties">
+                                        <div class="card-body">
+                                            <div class="list-group">
+                                                <samll class="text-muted mb-2">*Tapez sur la question pour voir la réponse proposée</samll>
+                                                <ol class="p-0">
+                                                    <div class="list-group-item list-group-item-action">
+                                                        <div href="#" class="">
+                                                            <li class="ml-3">
+                                                                Exemple de question ...
+                                                            </li>
+                                                        </div>
+                                                        <div class="mt-2 bg-info text-white p-2">
+                                                            <h5>Réponse :</h5>
+                                                            <p class="m-0">Exemple de réponse ...</p>
+                                                            <div class="d-flex justify-content-end">
+                                                                <button class="btn btn-light btn-sm" type="button">Proposez une autre solution</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <a href="#" class="list-group-item list-group-item-action">
+                                                        <li class="ml-3">
+                                                            Exemple de question ...
+                                                        </li>
+                                                    </a>
+                                                    <a href="#" class="list-group-item list-group-item-action">
+                                                        <li class="ml-3">
+                                                            Exemple de question ...
+                                                        </li>
+                                                    </a>
+                                                </ol>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-header py-1" id="partie_2">
+                                        <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                            <h5>
+                                                Partie 2:
+                                            </h5>
+                                        </button>
+                                    </div>
+                                    <div id="collapseTwo" class="collapse" aria-labelledby="partie_2" data-parent="#parties">
+                                        <div class="card-body">
+                                            Some placeholder content for the second accordion panel. This panel is hidden by default.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="eighteen">
+                            <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+                        </div>
+                        <div class="tab-pane fade" id="seventeen">
+                            <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
